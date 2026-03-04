b25eb9fcfcdd4f09e135ac0e1ce0a018dfd3a955
MarcYoba <yobamarc1@gmail.com>
2026-03-04 12:01:54 +0100

    correction de la methode de recuperation du premier element du tableau

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -19,10 +19,10 @@ class EntrepriseController extends AbstractController
         $sql = 'SELECT * FROM vente_a ORDER BY id DESC';
         $vente = $abgroup->executeQuery($sql)->fetchAllAssociative();
         $nbvemnte = count($vente);
-        $vente = array_pop($vente); // Récupère la première vente (la plus récente)
+        $vente = array_shift($vente); // Récupère la première vente (la plus récente)
         $sql = 'SELECT * FROM agence ORDER BY id DESC';
         $agence = $abgroup->executeQuery($sql)->fetchAllAssociative();
-        $agence = array_pop($agence); // Récupère la première agence (la plus récente)
+        $agence = array_shift($agence); // Récupère la première agence (la plus récente)
        //dd($vente);
         return $this->render('entreprise/index.html.twig', [
             'controller_name' => 'EntrepriseController',
