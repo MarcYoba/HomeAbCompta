8f89317e9b4585e42365504fe725c8c12a718600
MarcYoba <yobamarc1@gmail.com>
2026-03-04 11:57:48 +0100

    debogage des nom de la table de vente

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -16,14 +16,14 @@ class EntrepriseController extends AbstractController
         $abgroup = $em->getConnection();
         $bd+=1;
 
-        $sql = 'SELECT * FROM vente ORDER BY id DESC';
+        $sql = 'SELECT * FROM vente_a ORDER BY id DESC';
         $vente = $abgroup->executeQuery($sql)->fetchAllAssociative();
         $nbvemnte = count($vente);
         $vente = array_pop($vente); // Récupère la première vente (la plus récente)
         $sql = 'SELECT * FROM agence ORDER BY id DESC';
         $agence = $abgroup->executeQuery($sql)->fetchAllAssociative();
         $agence = array_pop($agence); // Récupère la première agence (la plus récente)
-       dd($vente);
+       //dd($vente);
         return $this->render('entreprise/index.html.twig', [
             'controller_name' => 'EntrepriseController',
             'vente' => $vente,
