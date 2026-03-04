1ec1493335a5fc9ebc698ba6c780bd344a85298e
MarcYoba <yobamarc1@gmail.com>
2026-03-04 11:49:27 +0100

    creation de la possibilite de sectionner un site web distant

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $bd = 0;
+        $abgroup = $em->getConnection();
+        $bd+=1;
+
+        $sql = 'SELECT * FROM vente ORDER BY id DESC';
+        $vente = $abgroup->executeQuery($sql)->fetchAllAssociative();
+        $nbvemnte = count($vente);
+        $vente = array_pop($vente); // Récupère la première vente (la plus récente)
+        $sql = 'SELECT * FROM agence ORDER BY id DESC';
+        $agence = $abgroup->executeQuery($sql)->fetchAllAssociative();
+        $agence = array_pop($agence); // Récupère la première agence (la plus récente)
+       //dd($vente);
         return $this->render('entreprise/index.html.twig', [
             'controller_name' => 'EntrepriseController',
+            'vente' => $vente,
+            'agence' => $agence,
+            'bd' => $bd,
+            'nbvente' => $nbvemnte,
         ]);
     }
 }
